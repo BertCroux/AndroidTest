627313cb1b7267a44fe70d9fd455cc023b35ad72
Mathias Bolle <mbolle2310@protonmail.com>
2022-10-25 11:17:05 +0200

    feat: setup navigation for menu

--- app/src/main/java/com/example/squads/MainActivity.kt
+++ app/src/main/java/com/example/squads/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.squads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.squads.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //TODO -> binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
