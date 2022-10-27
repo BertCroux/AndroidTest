1423a143df603b78e18b5012b49e37bc649f5de2
Mathias Bolle <mbolle2310@protonmail.com>
2022-10-27 17:45:16 +0200

    refactor: ID's of the navigation and menu

--- app/src/main/java/com/example/squads/MainActivity.kt
+++ app/src/main/java/com/example/squads/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.squads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.squads.databinding.ActivityMainBinding
@@ -21,34 +18,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = DataBindingUtil.inflate(layoutInflater)
-        setContentView(R.layout.activity_main) //temp solution
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navController = this.findNavController(R.id.nav_host_fragment_activity_main)
-
-        Log.d("MainActivity", navController.toString());
-
-        // Setup the bottom navigation view with navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
-        bottomNavigationView.setupWithNavController(navController)
-
-        // Setup the ActionBar with navController and 3 top level destinations
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.home, R.id.reservation)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        //setupMenu()
-
-
+        setupMenu()
     }
 
     /**
      * handles the bottom navigation
      */
     private fun setupMenu() {
+        // Setup the bottom navigation view with navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.setupWithNavController(navController)
 
-
+        // Setup the ActionBar with navController and 3 top level destinations
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home, R.id.reservations)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
-
 }
