63605c40c744046366e1c52d7aa88406245b5bdb
Mathias Bolle <mbolle2310@protonmail.com>
2022-10-27 17:16:25 +0200

    fix: navigation between home and reservation
    NOTE: data binding is removed by the main activity

--- app/src/main/java/com/example/squads/MainActivity.kt
+++ app/src/main/java/com/example/squads/MainActivity.kt
@@ -2,32 +2,53 @@ package com.example.squads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.squads.databinding.ActivityMainBinding
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.squads.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //TODO -> binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val navView: BottomNavigationView = binding.navView
+        //binding = DataBindingUtil.inflate(layoutInflater)
+        setContentView(R.layout.activity_main) //temp solution
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment
-            )
+
+        navController = this.findNavController(R.id.nav_host_fragment_activity_main)
+
+        Log.d("MainActivity", navController.toString());
+
+        // Setup the bottom navigation view with navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.setupWithNavController(navController)
+
+        // Setup the ActionBar with navController and 3 top level destinations
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home, R.id.reservation)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        //setupMenu()
+
+
     }
+
+    /**
+     * handles the bottom navigation
+     */
+    private fun setupMenu() {
+
+
+    }
+
 }
