4f795dcd4b4d28629ec39d6c0a0cdd3a622b6523
Remco <79316889+remcodesmedt@users.noreply.github.com>
2022-11-09 20:35:20 +0100

    function to map measurements
    based on the type, this function will map the measurements to a list of pairs (valuesForGraph) that contain the value and date.
    valuesForGraph will be used to create the graph

--- app/src/main/java/com/example/squads/screens/myhealth/MyHealthGraphsFragment.kt
+++ app/src/main/java/com/example/squads/screens/myhealth/MyHealthGraphsFragment.kt
@@ -1,20 +1,26 @@
 package com.example.squads.screens.myhealth
 
+import android.content.res.Resources.NotFoundException
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.squads.R
 import com.example.squads.databinding.FragmentMyHealthGraphsBinding
+import kotlinx.datetime.LocalDateTime
 
 class MyHealthGraphsFragment : Fragment() {
+
+    lateinit var measurements: List<Measurement>
+
+    //the list of values to display
+    lateinit var valuesForGraph: List<Pair<Double, LocalDateTime>>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,7 +49,42 @@ class MyHealthGraphsFragment : Fragment() {
             }
         })
 
+
+        //get the measurements from the viewmodel
+        myHealthViewModel.measurements.observe(viewLifecycleOwner, Observer {
+            measurements = it
+        })
+
+
+        //when the button is clicked, a type is sent along with it as string and is stored in the viewmodel
+        //so when the type in the viewmodel changes, the measurements have to be mapped to the right type
+        // in this observer function
+        myHealthViewModel.typeDataGraph.observe(viewLifecycleOwner, Observer { type ->
+            Log.i("graphs", type ?: "niks")
+
+            if (type != null) {
+                valuesForGraph = mapMeasurements(type)
+                valuesForGraph.forEach {
+                    Log.i("graphs", it.toString())
+                }
+            }
+        })
+
         return binding.root
+    }
+
+    private fun mapMeasurements(type: String): List<Pair<Double, LocalDateTime>> {
+        //this returns a list of pairs (tuples) that contain the value and the date
+        when (type) {
+            "weight" -> return measurements.map { Pair(it.Weight, it.MeasuredOn) }
+            "fat" -> return measurements.map { Pair(it.FatPercentage, it.MeasuredOn) }
+            "muscle" -> return measurements.map { Pair(it.MusclePercentage, it.MeasuredOn) }
+            "waist" -> return measurements.map { Pair(it.WaistCircumference, it.MeasuredOn) }
+            "bmi" -> print("apart geval") //TODO BMIshit
+            else -> {
+                throw NotFoundException()
+            }
+        }
     }
 
 }
