9a1ef5005833a0e03fc68f8dfe32a282d0b3e9fb
Mathias Bolle <mbolle2310@protonmail.com>
2022-11-12 19:55:44 +0100

    refactor data class with camelcase

--- app/src/main/java/com/example/squads/screens/sessions/Session.kt
+++ app/src/main/java/com/example/squads/screens/sessions/Session.kt
@@ -2,5 +2,5 @@ package com.example.squads.screens.sessions
 
 import kotlinx.datetime.LocalDateTime
 
-data class Session(val StartDate: LocalDateTime, val EndDate: LocalDateTime,
-                       val TypeOfSession: String, val Trainer: String, val SpotsLeft: Number)
+data class Session(val startDate: LocalDateTime, val endDate: LocalDateTime,
+                   val typeOfSession: String, val trainer: String, val spotsLeft: Number)
