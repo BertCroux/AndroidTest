e4521a3dd5782018c3027031b8095e5be3750fe6
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2022-12-02 12:55:04 +0100

    icon & initial auth0

--- app/src/main/java/com/example/squads/MainActivity.kt
+++ app/src/main/java/com/example/squads/MainActivity.kt
@@ -8,6 +8,11 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
 import androidx.viewpager.widget.ViewPager
 import com.example.squads.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,11 +22,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var account: Auth0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        account = Auth0(
+            "cnwbRWPWGfZZehbFA0XDstnpHFUm2Cdt",
+            "dev-gfralrybz0kc8g4m.us.auth0.com"
+        )
+        loginWithBrowser()
         navController = this.findNavController(R.id.nav_host_fragment_activity_main)
         setupMenu()
 
@@ -43,5 +54,27 @@ class MainActivity : AppCompatActivity() {
             setOf(R.id.home, R.id.reservations, R.id.settings)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun loginWithBrowser() {
+        // Setup the WebAuthProvider, using the custom scheme and scope.
+
+        WebAuthProvider.login(account)
+            .withScheme("demo")
+            .withScope("openid profile email")
+            // Launch the authentication passing the callback where the results will be received
+            .start(this, object : Callback<Credentials, AuthenticationException> {
+                // Called when there is an authentication failure
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                }
+
+                // Called when authentication completed successfully
+                override fun onSuccess(credentials: Credentials) {
+                    // Get the access token from the credentials object.
+                    // This can be used to call APIs
+                    val accessToken = credentials.accessToken
+                }
+            })
     }
 }
