a7207a7ca6946d745b57b739aa1b8027a2cec749
Mathias Bolle <mbolle2310@protonmail.com>
2022-12-02 14:53:19 +0100

    add logger

--- app/src/main/java/com/example/squads/network/MeasurementApiService.kt
+++ app/src/main/java/com/example/squads/network/MeasurementApiService.kt
@@ -5,6 +5,7 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -14,10 +15,14 @@ private const val BASE_URL =
     "https://squadsacceptancea01.azurewebsites.net/measurements/"
 
 private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
+    //.add(KotlinJsonAdapterFactory())
     .build()
 
+
+private val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
 private val client = OkHttpClient.Builder()
+    .addInterceptor(logger)
     .build()
 
 private val retrofit = Retrofit.Builder()
